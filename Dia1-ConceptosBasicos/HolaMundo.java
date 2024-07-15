826c33213cbf25dbe6dae325d2a28b9760df6a89
unciafidelis <unciafidelis@gmail.com>
2024-07-15 11:05:39 -0600

    Update HolaMundo.java

--- Dia1-ConceptosBasicos/HolaMundo.java
+++ Dia1-ConceptosBasicos/HolaMundo.java
@@ -1,7 +1,7 @@
 public class HolaMundo {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //ovalo inicio
         //La siguiente linea manda a imprimir en terminal el string ¡Hola mundo!
-        System.out.println("¡Hola Mundo!");
-    }
+        System.out.println("¡Hola Mundo!"); // símbolo doc para imprimir en pantalla
+    }//ovalo fin
 }
 
